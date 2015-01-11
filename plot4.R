2e81ab820b12b384b4770679629d329c2c026729
marcos <mcosmr@gmail.com>
2015-01-12 00:14:16 +0100

    update project 1 Exdata
    update files plot4.png and plot4.R

--- plot4.R
+++ plot4.R
@@ -38,7 +38,7 @@ plot(x, y4, type = "l", ylab = " Voltage ", xlab = "datetime")
 plot(x, y1, type = "l", ylab = " Energy sub metering ", xlab = "")
 lines(x, y2, col = "red")
 lines(x, y3, col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty =c(1,1,1), col= c("black", "red", "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col= c("black", "red", "blue"), bty = "n")
 
 plot(x, y5, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
 dev.off()
